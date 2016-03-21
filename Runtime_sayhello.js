4dab9cbdc639d1e0940be81a42f1090b3b866a31
aklef <andreas@klef.ca>
2016-03-21 15:44:08 -0400

    Whitespace changes

--- Runtime_sayhello.js
+++ Runtime_sayhello.js
@@ -1,6 +1,6 @@
-(function(skuid)
+(function (skuid)
 {
-   skuid.componentType.register("mblazonry__sayhello", function(element, xmlDef)
+   skuid.componentType.register("mblazonry__sayhello", function (element, xmlDef)
    {
       element.addClass("hello-content").html("<b>Hello " + xmlDef.attr("person") + "</b>");
    });
