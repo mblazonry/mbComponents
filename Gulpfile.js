edbb05957320e56ff1e151168618882c4b4a9ff9
aklef <andreas@klef.ca>
2016-05-06 16:00:12 -0400

    Setup gulp for automated task workflows on dev and release builds.

---
+++ Gulpfile.js
@@ -0,0 +1,132 @@
+/* jshint node: true */
+/*********************/
+
+// include gulp & grunt
+var gulp = require('gulp');
+
+require('gulp-grunt')(gulp); // add all the gruntfile tasks to gulp
+
+// include plug-ins
+var merge = require('merge-stream'),
+   jshint = require("gulp-jshint"),
+   uglify = require("gulp-uglify"),
+   cleanCSS = require('gulp-clean-css'),
+   jsonminify = require('gulp-jsonminify'),
+   zip = require('gulp-zip'),
+   gutil = require('gulp-util'),
+   clean = require('gulp-clean'),
+   stripCode = require('gulp-strip-code'),
+   header = require('gulp-header');
+
+///////////
+// Tasks //
+///////////
+
+// create a default task and just log a message
+gulp.task('default', function ()
+{
+   return gutil.log('Gulp is running!');
+});
+
+/**
+ * Lint files project source files using jshint for errors and fail if any are found.
+ */
+gulp.task('lint', function ()
+{
+   gulp.src('./components/**/*.js') // path to your files
+   .pipe(jshint())
+      .pipe(jshint.reporter()); // Dump results
+});
+
+/**
+ * Remove old dev files from directory.
+ */
+gulp.task('clean-dev', function ()
+{
+   return gulp.src('./*-dev.zip',
+      {
+         read: false
+      })
+      .pipe(clean());
+});
+
+/**
+ * Remove old release files from directory.
+ */
+gulp.task('clean-min-release', function ()
+{
+   return gulp.src('./*-min-release.zip',
+      {
+         read: false
+      })
+      .pipe(clean());
+});
+
+
+
+////////////
+// Builds //
+////////////
+
+gulp.task('build-min-timer', ['clean-min-release', 'lint'], function ()
+{
+   // minify-js
+   var min_js = gulp.src('./components/*_timer/*.js')
+      .pipe(uglify());
+
+   //minify-css
+   var min_css = gulp.src('./components/*_timer/*.css')
+      // .pipe(cleanCSS())
+      .pipe(cleanCSS(
+      {
+         debug: true
+      }, function (details)
+      {
+         console.log(details.name + ': ' + details.stats.originalSize);
+         console.log(details.name + ': ' + details.stats.minifiedSize);
+      }));
+
+   // combine
+   var min_src = merge(min_js, min_css);
+
+   // TODO refactor this out
+   // get data from package.json
+   var pkg = require('./package.json');
+   var banner = ['/**',
+      ' * <%= pkg.name %> - <%= pkg.description %>',
+      ' * @version v<%= pkg.version %>',
+      //' * @link <%= pkg.link %>',
+      ' * @license <%= pkg.license %>',
+      ' * @author <%= pkg.author %>',
+      ' */',
+      ''
+   ].join('\n');
+   // configs
+   var min_configs = gulp.src('./skuid_*.json')
+      // strip non-timer stuff
+      .pipe(stripCode(
+      {
+         start_comment: "start-build-timer",
+         end_comment: "end-build-timer"
+      }))
+      // minify configs
+      .pipe(jsonminify())
+      // append header to config files
+      .pipe(header(banner,
+      {
+         pkg: pkg
+      }));
+
+   // Zip all files
+   var zip_files = merge(min_src, min_configs)
+      .pipe(zip('./mblazonryComponents-min-release.zip'))
+      .pipe(gulp.dest('./'));
+
+   return zip_files;
+});
+
+gulp.task('build-dev', ['clean-dev', 'lint'], function ()
+{
+   return gulp.src(['./components/**/*.*', './skuid_*.json']).pipe(zip('./mblazonryComponents-dev.zip'))
+      .pipe(gulp.dest('./'));
+});
--- README.md
+++ README.md
@@ -10,6 +10,44 @@ Home of mBlazonry's primary custom component source!
 
 - Barry Scnell's old ["popup controller" custom component](https://community.skuidify.com/skuid/topics/popup-controller-component-disable-x-escape-key-and-hook-dialog-events) code from the skuid community forums.
 
+## Setup ##
+
+This section has two parts. You already did the initial setup part by downloading this repo.
+
+First part:
+
+
+1.  [download](https://nodejs.org/en/download/) and [install](http://blog.teamtreehouse.com/install-node-js-npm-windows) [Node.js](https://nodejs.org/en/) From the console you should now be able to run `npm`.
+
+2.  In the repo directory, run `npm install mblazonrycomponents`
+
+3.  That sets up the app, automatically grabbing all dependencies needed by the project and installing them to a node_modules folder locally.
+
+4.  If you run any of the automation now you'll get an error saying Gulp needs to be installed globally. So from the repo dir run `npm install gulp -g`
+
+5.  Once that's done installing, make sure `npm ls` returns a load of stuff.
+
+6.  Now you're good to go.
+
+Second, there are instructions for Sublime Text 3. These were tested only on Windows 7 64-bit round feb 2016. YMMV
+
+1.  If you don't have it already, [install Package Control](https://packagecontrol.io/installation), the sublime package manager.
+
+2. Now, we go about installing some packages:
+
+    [sublime-gulp](https://github.com/NicoSantangelo/sublime-gulp) (and here's [a guide](https://mijingo.com/blog/run-gulp-from-sublime-text)),
+	
+	[sublime-jshint](https://github.com/uipoet/sublime-jshint),
+
+    [sublime-jsdocs](https://github.com/spadgos/sublime-jsdocs) (reccommended),
+
+    [GitGutter](https://github.com/jisaacks/GitGutter) (reccommended)
+
+3. With all that installed, you should be able to right-click a file in the FOLDERS tree in Sublime's sidebar [right-click] → Gulp → "List Tasks to Run". That should show the various build options. Run the `default` build and do `Ctrl + ~` on your keyboard to show the console in Sublime, and observe the output.
+
+4. Assuming all went well you should be good to develop mBlazonry's components and use automated builds!
+    
+
 ## BSD License ##
 
 
@@ -18,12 +56,15 @@ All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
-    * Redistributions of source code must retain the above copyright
+   
+
+
+1. Redistributions of source code must retain the above copyright
       notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright
+2. Redistributions in binary form must reproduce the above copyright
       notice, this list of conditions and the following disclaimer in the
       documentation and/or other materials provided with the distribution.
-    * Neither the name of the mBlazonry nor the
+3. Neither the name of the mBlazonry nor the
       names of its contributors may be used to endorse or promote products
       derived from this software without specific prior written permission.
 
