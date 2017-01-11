6dec145a057d283a5ec9d7522cf91923f810e93f
Remi Gelinas <remigelinas1@gmail.com>
2017-01-11 10:12:46 -0500

    Moves individual components into Git submodules

--- components/mblazonry_sayHello/Runtime_sayHello.js
+++
@@ -1,9 +0,0 @@
-(function (skuid)
-{
-   'use strict';
-
-   skuid.componentType.register("mblazonry__sayhello", function (element, xmlDef)
-   {
-      element.addClass("hello-content").html("<b>Hello " + xmlDef.attr("person") + "</b>");
-   });
-})(window.skuid);
