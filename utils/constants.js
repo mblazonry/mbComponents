d6ef83db2b6f753cd68820983aeb2b2246b0f362
Remi Gelinas <remigelinas1@gmail.com>
2017-02-28 02:59:23 -0500

    Move all common constants to separate module

---
+++ utils/constants.js
@@ -0,0 +1,12 @@
+const Path = require('path');
+
+var exports = module.exports = {};
+
+exports.CONTEXT_DIRECTORY = Path.resolve('.');
+exports.BUILD_DIRECTORY = Path.resolve(exports.CONTEXT_DIRECTORY, 'build');
+exports.BUNDLE_DIRECTORY = Path.resolve(exports.BUILD_DIRECTORY, 'bundle');
+exports.CONFIG_DIRECTORY = Path.resolve(exports.BUILD_DIRECTORY, 'config');
+exports.COMPONENT_DIRECTORY = Path.resolve(exports.CONTEXT_DIRECTORY, 'components');
+exports.ORG_DIRECTORY = Path.resolve(exports.CONTEXT_DIRECTORY, 'orgs'),
+exports.PACKAGE_ID = 'mblazonrybeta';
+exports.PACKAGE_NAME = 'mbComponents';
