2fe0b593308a5d82f16b090368df32e6345bc28a
Remi Gelinas <remigelinas1@gmail.com>
2017-03-01 12:28:03 -0500

    Change package name

--- utils/constants.js
+++ utils/constants.js
@@ -10,4 +10,4 @@ exports.CONFIG_DIRECTORY = Path.resolve(exports.BUILD_DIRECTORY, 'config');
 exports.COMPONENT_DIRECTORY = Path.resolve(exports.CONTEXT_DIRECTORY, 'components');
 exports.ORG_DIRECTORY = Path.resolve(exports.CONTEXT_DIRECTORY, 'orgs'),
 exports.PACKAGE_ID = 'mblazonrybeta';
-exports.PACKAGE_NAME = 'mbComponents';
+exports.PACKAGE_NAME = 'mbComponentsBeta';
