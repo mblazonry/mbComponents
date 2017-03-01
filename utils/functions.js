3c73ff5ca861eea45ab002e0a3e61bc4a70eb334
Remi Gelinas <remigelinas1@gmail.com>
2017-03-01 12:28:33 -0500

    Move common functions into separate module

---
+++ utils/functions.js
@@ -0,0 +1,22 @@
+/** @module Functions */
+const Fs = require('fs'),
+    Path = require('path'),
+    Constants = require('./constants');
+
+/** @function forComponent
+ * Executes a callback for every Skuid component found in the environment, passing the component name into the callback.
+ *
+ * @callback componentCallback
+ * @param {string} componentName
+ */
+function forComponent(f) {
+    var files = Fs.readdirSync(Constants.COMPONENT_DIRECTORY).filter(file => {
+        return Fs.statSync(Path.resolve(Constants.COMPONENT_DIRECTORY, file)).isDirectory();
+    });
+
+    files.forEach(f);
+}
+
+var exports = module.exports = {
+    forComponent: forComponent
+};
